7776bc5594ff438144c73e7c9e980152c27d6785
Karl Bucad <bucadkarl@gmail.com>
2026-07-04 15:39:55 -0400

    Add application creation from frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [applications, setApplications] = useState([])
+  
+  const [company, setCompany] = useState("")
+  const [role, setRole] = useState("")
+  const [status, setStatus] = useState("")
 
   async function handleLogin(event) {
     event.preventDefault()
@@ -21,20 +25,6 @@ function App() {
       body: formData,
     })
 
-    async function fetchApplications() {
-      const token = localStorage.getItem("token")
-
-      const response = await fetch("http://127.0.0.1:8000/applications", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      const data = await response.json()
-
-      setApplications(data)
-    }
-
     const data = await response.json()
 
     localStorage.setItem("token", data.access_token)
@@ -42,9 +32,49 @@ function App() {
     fetchApplications()
   }
 
+  async function fetchApplications() {
+    const token = localStorage.getItem("token")
+
+    const response = await fetch("http://127.0.0.1:8000/applications", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const data = await response.json()
+
+    setApplications(data)
+  }
+
+  async function handleAddApplication(event) {
+    event.preventDefault()
+  
+    const token = localStorage.getItem("token")
+  
+    await fetch("http://127.0.0.1:8000/applications", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        company: company,
+        role: role,
+        status: status,
+      }),
+    })
+  
+    setCompany("")
+    setRole("")
+    setStatus("")
+  
+    fetchApplications()
+  }
+
   return (
     <div>
       <h1>Internship Tracker</h1>
+  
       <h2>Login</h2>
   
       <form onSubmit={handleLogin}>
@@ -63,6 +93,33 @@ function App() {
         />
   
         <button type="submit">Login</button>
+      </form>
+  
+      <h2>Add Application</h2>
+  
+      <form onSubmit={handleAddApplication}>
+        <input
+          type="text"
+          placeholder="Company"
+          value={company}
+          onChange={(event) => setCompany(event.target.value)}
+        />
+  
+        <input
+          type="text"
+          placeholder="Role"
+          value={role}
+          onChange={(event) => setRole(event.target.value)}
+        />
+  
+        <input
+          type="text"
+          placeholder="Status"
+          value={status}
+          onChange={(event) => setStatus(event.target.value)}
+        />
+  
+        <button type="submit">Add Application</button>
       </form>
   
       <h2>My Applications</h2>
